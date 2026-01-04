19c8353cc4609479748c254753056921a0e3f943
VishnuKosuri12 <vishnu.kosuri121212@gmail.com>
2026-01-03 23:51:29 -0500

    l2

--- infra/provider.tf
+++ infra/provider.tf
@@ -6,8 +6,15 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "state-bucket-3128-4647-3135"
+    key     = "lambda-rotation/terraform.tfstate"
+    region  = "us-east-2" # Ohio (Where the bucket is)
+    encrypt = true
+  }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = var.aws_region # us-east-1 (Where the resources go)
 }
